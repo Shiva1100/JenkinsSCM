6ee2a1769893a02ae7d58083eef926ea71fc2dfb
Intelli Pride <intellipride@gmail.com>
2024-06-27 19:06:50 +0530

    adding few files

--- JenkinsBuild/src/test/java/Testng/JenkinsBuild/LoginTest.java
+++ JenkinsBuild/src/test/java/Testng/JenkinsBuild/LoginTest.java
@@ -1,16 +1,23 @@
 package Testng.JenkinsBuild;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
 
 public class LoginTest {
+	WebDriver driver;
 	@Test (priority = 1)
 	public void oneMethod() {
-		System.out.println("Launching browser");
+		
+		driver = new ChromeDriver();
+		System.out.println("Launching browser successfully");
 	}
 	@Test (priority = 2,timeOut = 3000)
 	public void testTwoMethod() throws InterruptedException {
-		Thread.sleep(5000);
-		System.out.println("Enter URL");
+//		Thread.sleep(5000);
+		driver.get("https://www.google.com");
+		System.out.println("Entered URL successfully");
 	}
 	
 	@Test (priority = 3)
@@ -21,6 +28,11 @@ public class LoginTest {
 	@Test (priority = 4)
 	public void singInMethod() {
 		System.out.println("Click on signIn and It is successful");
+	}
+	@AfterSuite
+	public void closeBrowser() {
+		driver.quit();
+		System.out.println("closed the browser successfully");
 	}
 
 }
