335fb393334a2f94d206ba66f709a7b0c0afcc4e
Intelli Pride <intellipride@gmail.com>
2024-06-27 10:27:02 +0530

    changed class name contains test

--- JenkinsBuild/src/test/java/Testng/JenkinsBuild/LoginTest.java
+++ JenkinsBuild/src/test/java/Testng/JenkinsBuild/LoginTest.java
@@ -2,23 +2,23 @@ package Testng.JenkinsBuild;
 
 import org.testng.annotations.Test;
 
-public class MainClass {
-	@Test(priority = 1)
+public class LoginTest {
+	@Test (priority = 1)
 	public void oneMethod() {
 		System.out.println("Launching browser");
 	}
-	@Test(priority = 2,timeOut = 3000)
-	public void twoMethod() throws InterruptedException {
+	@Test (priority = 2,timeOut = 3000)
+	public void testTwoMethod() throws InterruptedException {
 		Thread.sleep(5000);
 		System.out.println("Enter URL");
 	}
 	
-	@Test(priority = 3)
+	@Test (priority = 3)
 	public void threeMethod() {
 		System.out.println("Locate login page and enter data");
 	}
 	
-	@Test(priority = 4)
+	@Test (priority = 4)
 	public void singInMethod() {
 		System.out.println("Click on signIn and It is successful");
 	}
